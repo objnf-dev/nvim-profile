e00048d32ddf1353624afa0c49160bf860a8049d
ObjectNotFound <xml@live.com>
2023-05-24 17:23:29 +0800

    Add missing function has_words_before

--- init.lua
+++ init.lua
@@ -258,6 +258,10 @@ lspconfig.grammarly.setup({})
 lspconfig.remark_ls.setup({})
 
 -- 初始化代码补全
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 cmp.setup({
